cf5b44101a867c70b6e286afb99f0b0536c2d297
Jason Pruski <jmpruski@gmail.com>
2019-10-24 13:15:31 -0700

    added some components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './Components/login/login.component';
+import { NavbarComponent } from './Components/navbar/navbar.component';
+import { StoreComponent } from './Components/store/store.component';
+import { RewardComponent } from './Components/store/reward/reward.component';
+import { AdjustmentsComponent } from './Components/adjustments/adjustments.component';
+import { DashboardComponent } from './Components/dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    NavbarComponent,
+    StoreComponent,
+    RewardComponent,
+    AdjustmentsComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
