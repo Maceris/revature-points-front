1db9140c100bd4c19be8f6baa6c6287f7f651669
maceris <KingOfL33tGaming@gmail.com>
2019-10-27 16:13:10 -0400

    Add associate model

---
+++ src/app/Models/associate.ts
@@ -0,0 +1,22 @@
+import { Trainer } from './trainer';
+
+export class Associate {
+    
+    a_id: number;
+    username: string;
+    password: string;
+    balance: number;
+    f_name: string;
+    l_name: string;
+    trainer: Trainer;
+
+    constructor(a_id: number, username: string, password: string, balance: number, f_name: string, l_name: string, trainer: Trainer) {
+       this.a_id = a_id;
+       this.username = username;
+       this.password = password;
+       this.balance = balance;
+       this.f_name = f_name;
+       this.l_name = l_name;
+       this.trainer = trainer;
+    }
+}
