fff62641ad99ba2e8529343f45766657a540a9bc
maceris <KingOfL33tGaming@gmail.com>
2019-10-27 14:40:19 -0400

    Add some framework for dynamic rewards in store

--- src/app/Components/store/reward/reward.component.ts
+++ src/app/Components/store/reward/reward.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-reward',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./reward.component.css']
 })
 export class RewardComponent implements OnInit {
+
+  @Input() name: string = "Item";
 
   constructor() { }
 
--- src/app/Components/store/store.component.ts
+++ src/app/Components/store/store.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StoreComponent implements OnInit {
 
+  rewards = [{name: "Sample"}, {name: "Test"}, {name: "Example"}];
+
   constructor() { }
 
   ngOnInit() {
